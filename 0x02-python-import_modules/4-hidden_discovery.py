14e7263d8febf4c123c99f554b572c4851c49498
herdeybayor <shereefdeen4real@gmail.com>
2023-06-09 05:46:38 +0100

    fix: minor bug

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,11 +2,11 @@
 """ 4-hidden_discovery.py """
 import hidden_4
 
-def main():
-    """ main """
+def discover():
+    """ discover """
     for name in dir(hidden_4):
-        if name[0] != '_':
+        if name[:2] != "__":
             print(name)
 
 if __name__ == "__main__":
-    main()
+    discover()
