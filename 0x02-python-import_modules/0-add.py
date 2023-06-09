5a6a12aada61d5363d789fe1328687ff3b75a182
herdeybayor <shereefdeen4real@gmail.com>
2023-06-09 05:55:52 +0100

    fix

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-"""This module contains a function that adds two integers"""
 from add_0 import add
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     a = 1
     b = 2
-    print("{} + {} = {}".format(a, b, add(a, b)))
+    c = add(a, b)
+
+    print('{} + {} = {}'.format(a, b, c))
