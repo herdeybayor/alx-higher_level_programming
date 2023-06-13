5ab76bd972ce65324ca4a1461b9866f439c123ef
herdeybayor <shereefdeen4real@gmail.com>
2023-06-13 02:54:06 +0100

    fix: minor bug

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+
 def multiple_returns(sentence):
-    if sentence is None:
+    """
+    Returns the length of a string and its first character.
+    """
+    if sentence == "":
         return (0, None)
-    else:
-        return (len(sentence), sentence[0])
+    return (len(sentence), sentence[0])
