d5014f74cce098fff5cdd188bc09c52aec6317f2
herdeybayor <shereefdeen4real@gmail.com>
2023-07-06 06:03:54 +0100

    task-1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Square class"""
+
+
+class Square:
+    """ Represents a square """
+    def __init__(self, size):
+        """ Initiate a new square.
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size
