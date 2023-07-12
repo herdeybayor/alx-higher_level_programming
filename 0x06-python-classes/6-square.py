10a08b0eb7fac91740f7de3efc1a6ab63d638b45
herdeybayor <shereefdeen4real@gmail.com>
2023-07-12 03:00:57 +0100

    fix

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,27 +1,36 @@
 #!/usr/bin/python3
-""" Define a class Square """
+"""Define a class Square."""
 
 
 class Square:
-    """ Represent a square """
+    """Represent a square."""
+
     def __init__(self, size=0, position=(0, 0)):
-        """ Initialize Square class
+        """Initialize a new square.
         Args:
-            size (int): size of a side of the square
-            position (int, int): position of the square
+            size (int): The size of the new square.
+            position (int, int): The position of the new square.
         """
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
-        """Get/set the current size of the square"""
-        return self.__size
+        """Get/set the current size of the square."""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
 
     @property
     def position(self):
-        """Get/set the current position of the square"""
-        return self.__position
+        """Get/set the current position of the square."""
+        return (self.__position)
 
     @position.setter
     def position(self, value):
@@ -32,17 +41,9 @@ class Square:
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
-    @size.setter
-    def size(self, value):
-        if not isinstance(value, int):
-            raise TypeError("size must be an integer")
-        elif value < 0:
-            raise ValueError("size must be >=0")
-        self.__size = value
-
     def area(self):
-        """Returns the current square area"""
-        return self.__size ** 2
+        """Return the current area of the square."""
+        return (self.__size * self.__size)
 
     def my_print(self):
         """Print the square with the # character."""
